83e001302efb00b2cfac28fd77359e656eb367dd
Kris Kowal <kris@cixar.com>
2024-10-16 14:46:00 -0700

    Fix order and capitalization of sidebar

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,19 +17,20 @@ export default defineConfig({
       // social: {
       // 	github: 'https://github.com/withastro/starlight',
       // },
-      // sidebar: [
-      // 	{
-      // 		label: 'Guides',
-      // 		items: [
-      // 			// Each item here is one entry in the navigation menu.
-      // 			{ label: 'Example Guide', slug: 'guides/example' },
-      // 		],
-      // 	},
-      // 	{
-      // 		label: 'Reference',
-      // 		autogenerate: { directory: 'reference' },
-      // 	},
-      // ],
+      sidebar: [
+        {
+          label: "Challenge",
+          autogenerate: { directory: "challenge" },
+        },
+        {
+          label: "Console",
+          slug: "console",
+        },
+        {
+          label: "Blog",
+          autogenerate: { directory: "blog" },
+        },
+      ],
     }),
   ],
   redirects: {
